122b33831296ba72ffe728323f88e57dd7b2d66f
Alexander Blaufuss <alexander.blaufuss@statworx.com>
2020-12-15 17:52:48 +0100

    Switched from requests to werkzeugs test-client.

--- test_app.py
+++ test_app.py
@@ -1,16 +1,27 @@
 import requests
 import pytest
+from app import app
 
 
 @pytest.fixture
 def get_request():
-    r = requests.get(url='http://0.0.0.0:5000')
+    # r = requests.get(url='http://0.0.0.0:5000')
+    r =  app.test_client()
     return r
 
 def test_status_code(get_request):
+    """
     r = get_request
-    assert r.status_code == 200, 'Status is 200.'
+    status = r.status_code
+    """
+    r = get_request.get('/')
+    status = int(r.status.split()[0])
+    assert status == 200, 'Status is 200.'
 
 def test_message(get_request):
+    """
     r = get_request
-    assert r.text == 'Data Engineer Schulung: Herzlich Willkommen Teilnehmer!'
+    text = r.text
+    """
+    text = get_request.get('/').get_data(as_text=True)
+    assert text == 'Data Engineer Schulung: Herzlich Willkommen Teilnehmer!'
